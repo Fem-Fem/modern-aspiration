e369cc582ab66fcdf6bc7a7caec5600da150ff45
Kim Gonzales <Kimgo589@gmail.com>
2018-11-15 14:34:17 -0500

    build GoalsContainer, GoalsList and Goals

--- client/src/components/Goals/Goal.js
+++ client/src/components/Goals/Goal.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 export default class Goal extends Component {
   render(){
+    const { aim, strategy, category, objectives } = this.props;
+
     return(
-      <div>
-        {this.props.aim}
+      <div className="Goal">
+        <h3>Aspiration: {aim}</h3>
+        <h4>Strategy: {strategy}</h4>
+        <h5>Category: {category}</h5>
       </div>
     )
   }

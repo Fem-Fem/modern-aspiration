4791739170ed98c804bd98b0976d42395a453592
Kim Gonzales <Kimgo589@gmail.com>
2018-11-16 11:38:55 -0500

    change seed data, reset db and delete unnecessary node_modules in root directory.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 goal1 = Goal.create(aim:"Become a Software Engineer", category: "Professional Development", strategy: "Learn to Code")
+goal2 = Goal.create(aim:"Get Fit", category: "Health", strategy: "Eat Healthier, pick up cycling")
 
 objective1 = Objective.create(description: "Finish Curriculum", goal_id: 1, completed: 0)
 objective2 = Objective.create(description: "Build 5 Projects", goal_id: 1, completed: 0)
 objective3 = Objective.create(description: "Build Portfolio", goal_id: 1, completed: 0)
+
+objective4 = Objective.create(description: "Invite Alex to go biking once a week", goal_id: 2, completed: 0)
+objective5 = Objective.create(description: "Kickbox 3x weekly", goal_id: 2, completed: 0)

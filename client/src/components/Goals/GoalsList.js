917829a3073993dd72e92cd7109c25f9639de1c6
Kim Gonzales <Kimgo589@gmail.com>
2018-11-16 18:53:22 -0500

    refactor GoalsList

--- client/src/components/Goals/GoalsList.js
+++ client/src/components/Goals/GoalsList.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Goal from './Goal';
 
 const GoalsList = ({ goals }) => (
   <div>
     <ol>
-    {goals.map((goal) => <li><Goal key={goal.id} aim={goal.aim} goal={goal} strategy={goal.strategy} category={goal.category} /></li> )}
+    {goals.map((goal) => (
+      <li>
+        <Goal key={goal.id} aim={goal.aim} goal={goal} strategy={goal.strategy} 
+          category={goal.category} objectives={goal.objectives} />
+      </li> ))}
     </ol>
   </div>
 );

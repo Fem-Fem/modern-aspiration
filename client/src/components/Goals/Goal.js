da419a43fd9ef66958f27081b05dd60a42ed9c57
Kim Gonzales <Kimgo589@gmail.com>
2018-11-16 18:48:10 -0500

    refactor Goalslist to use const instead of class

--- client/src/components/Goals/Goal.js
+++ client/src/components/Goals/Goal.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Goal = ({ aim, strategy, category, objectives }) => (
   <div className="Goal">
--- client/src/components/Goals/GoalsList.js
+++ client/src/components/Goals/GoalsList.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
-import Goal from './Goal'
+import Goal from './Goal';
 
+const GoalsList = ({ goals }) => (
+  <div>
+    <ol>
+    {goals.map((goal) => <li><Goal key={goal.id} aim={goal.aim} goal={goal} strategy={goal.strategy} category={goal.category} /></li> )}
+    </ol>
+  </div>
+);
 
-export default class GoalsList extends Component {
-  render(){
-    const { goals }  = this.props; 
-  
-    const listGoals = goals.map(g => {
-      return (
-        <li><Goal key={g.id} aim={g.aim} goal={g} strategy={g.strategy} category={g.category}/></li>
-      )
-    })
-    
-    return(
-      <ol>
-        {listGoals}
-      </ol>
-    )
-   
-  }
-}
-
+export default GoalsList;

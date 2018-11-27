c2062bdd37f10ea43db2f3559b69e7cf46e87e0a
Kim Gonzales <Kimgo589@gmail.com>
2018-11-27 11:36:04 -0500

    delete commented out code in Goal Component

--- client/src/components/Goals/Goal.js
+++ client/src/components/Goals/Goal.js
@@ -1,16 +1,5 @@
 import React, { Component } from 'react';
 
-// const Goal = ({ aim, strategy, category, objectives }) => (
-//   <div className="Goal">
-//     <h3>{aim}</h3>
-//     <strong>Strategy:</strong>
-//       <p>{strategy}</p>
-//     <strong>Category:</strong>
-//         <p>{category}</p>
-//     <button onClick={ () => this.props.deleteGoal(this.props.id)}>Delete</button>
-//   </div>
-// )
-
 class Goal extends Component {
   render(){
     const {aim, strategy, category, objectives } = this.props;

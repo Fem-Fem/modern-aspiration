7f66a77f62f012be0fb12545270c79d76cf0454e
Kim Gonzales <Kimgo589@gmail.com>
2018-11-27 10:04:12 -0500

    add completed column to goals table in rails api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_213259) do
+ActiveRecord::Schema.define(version: 2018_11_27_144139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_11_13_213259) do
     t.string "strategy"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "completed", default: 0
   end
 
   create_table "objectives", force: :cascade do |t|

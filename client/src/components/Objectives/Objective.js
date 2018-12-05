55fb378f27db0ec9bc0272fe4502b350ce776e9b
Kim Gonzales <Kimgo589@gmail.com>
2018-12-05 17:13:16 -0500

    change delete button to icon

--- client/src/components/Objectives/Objective.js
+++ client/src/components/Objectives/Objective.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Button, Checkbox } from 'semantic-ui-react';
+import { Button, Checkbox, Icon } from 'semantic-ui-react';
+
 
 
 export default class Objective extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state ={
       completed: false
@@ -14,12 +15,17 @@ export default class Objective extends Component {
     e.preventDefault();
     this.props.toggleCompleted(this.props.id)
   }
+
+  handleClick = e => {
+    e.preventDefault();
+    alert('clicked delete Icon!')
+  }
   
-  render(){
+  render() {
     return(
       <div>
-        <Checkbox checked={this.props.completed} onChange={(e) => this.handleCheck(e)} value={this.props.completed}/>{this.props.description}<br/>
-        <Button basic color='red' size='mini' onClick={() => this.props.deleteObjective(this.props.id)}> delete </Button>
+        <Icon className="pointer" name='delete' color='red' onClick={() => this.props.deleteObjective(this.props.id)} />
+        <Checkbox checked={this.props.completed} onChange={(e) => this.handleCheck(e)} value={this.props.completed}/>{this.props.description}        
       </div>
     )
   }

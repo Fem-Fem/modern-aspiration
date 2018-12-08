d629bbc1aa699bec1ea0957189ee787c8132426c
Kim Gonzales <Kimgo589@gmail.com>
2018-12-08 14:59:08 -0500

    add todo for home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Home = () => (
   <div className="home-page">
     <h4>Time's ticking, what's next on your <a href="/goals">list?</a></h4>
+    
+    <article>The benefits of Goal Tracking:</article>
 
   </div>
 )

bb9a1b36225dcf094e3478c119ac20ae5a8312e8
Kim Gonzales <Kimgo589@gmail.com>
2018-12-12 16:09:16 -0500

    remove whitespace

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,8 @@
 import React from 'react';
-
+s
 const Home = () => (
   <div className="home-page">
     <h4>Time's ticking, what's next on your <a href="/goals">list?</a></h4>
-    
-    <article>The benefits of Goal Tracking:</article>
-
   </div>
 )
 
